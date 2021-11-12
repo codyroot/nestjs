a8d8ef08fac457cbe3e82924016cb15d54afe0a7
agrossma <a.grossmann@eclever.io>
2021-11-12 10:29:07 +0100

    global exception

--- service_AAA/src/error.module.ts/error.module.ts
+++ service_AAA/src/error.module.ts/error.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ErrorController } from "./error.controller";
+import { APP_FILTER } from "@nestjs/core";
+import { GlobalExceptionFilter } from "./global-exception.filter";
 
 @Module({
     imports: [],
     exports: [],
     controllers: [ErrorController],
-    providers: [],
+    providers: [
+        {
+            // Hinweis -> Die Fehlerklasse wird hierdurch GLOBAL in allen anderen Modulen verwendet
+            provide: APP_FILTER,
+            useClass: GlobalExceptionFilter,
+        },
+    ],
 })
 export class ErrorModule {}
--- service_AAA/src/error.module.ts/global-exception.filter.ts
+++ service_AAA/src/error.module.ts/global-exception.filter.ts
@@ -8,11 +8,12 @@ import {
 import { Request, Response } from "express";
 
 @Catch()
-export class AllExceptionFilter implements ExceptionFilter {
+export class GlobalExceptionFilter implements ExceptionFilter {
     catch(exception: unknown, host: ArgumentsHost) {
         const context = host.switchToHttp();
         const response = context.getResponse<Response>();
         const request = context.getRequest<Request>();
+
         const status =
             exception instanceof HttpException
                 ? exception.getStatus()
@@ -21,11 +22,17 @@ export class AllExceptionFilter implements ExceptionFilter {
         const message =
             exception instanceof Error ? exception.message : "fehler...";
 
+        const type =
+            exception instanceof Error
+                ? (exception as unknown as FunctionConstructor).name
+                : "Unknown";
+
         response.status(status).json({
             statusCode: status,
             timestamp: new Date().toISOString(),
             path: request.url,
-            message,
+            message: `GLOBAL: ${message}`,
+            type
         });
     }
 }
